2055405aba36afb2a8f75abbe2c32e457eae11bf
ChelsiPatel2311 <chelsipatel2311@gmail.com>
2026-02-23 22:22:06 +0530

    add success

--- full_pyramid.py
+++ full_pyramid.py
@@ -1,22 +1,14 @@
-# practical 4A code 
 rows = 5
 
-for i in range(1, rows + 1):
-    
-    # Print spaces (for right alignment)
-    print(" " * (rows - i), end="")
-    
-    # Print alphabets
-    for j in range(i):
-        print(chr(65 + j), end=" ")
-    
+for i in range(rows):
+    # Print leading spaces
+    print(" " * i, end="")
+
+    # Print stars with space
+    for j in range(rows - i):
+        print("* ", end="")
+
     print()
-# Full Pyramid (Inverted)
-
-rows = 5
-
-for i in range(rows, 0, -1):
-    print("* " * i)
 # practical 4A code 
 rows = 5
 
